5a491e095ed38adb364826bb6076bab7936855c3
vikas2005-0607 <vikasmodanwal2395@gmail.com>
2026-09-11 14:54:59 +0530

    Update project attribution in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,4 +81,4 @@ if st.button("Show Model Feature Importance"):
     ax.set_title("XGBoost Global Feature Importance")
     st.pyplot(fig)
 
-st.markdown("<hr><span style='color: #FFD600;'> Mining ML Project by Achyant Shrivastava©️ 2025</span>", unsafe_allow_html=True)
+st.markdown("<hr><span style='color: #FFD600;'> Mining ML Project by vikas Modanwal©️ 2025</span>", unsafe_allow_html=True)
